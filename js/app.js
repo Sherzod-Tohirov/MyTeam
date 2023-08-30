1b248abaf3c9845e118d7ff1eaf1184cbf827ba4
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-08-30 17:22:55 +0500

    Error fixed

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ const elCloseBtn = document.querySelector('.js-close-btn');
 const elHeaderInner = document.querySelector('.js-header-inner');
 
 elBarBtn.addEventListener('click', (evt) => {
-  elHeaderInner.classList.toggle('move-nav');  
+  elHeaderInner.style.display = 'flex';
+  setTimeout(() => {
+    elHeaderInner.classList.toggle('move-nav'); 
+  }, 10); 
 });
 
 elCloseBtn.addEventListener('click', (evt) => {
-    elHeaderInner.classList.remove('move-nav');  
+    elHeaderInner.classList.remove('move-nav');
 });
